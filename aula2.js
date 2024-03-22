767eacae2206017a0c91ff73180aa48720d2d107
GustavoPinheir0 <164449358+GustavoPinheir0@users.noreply.github.com>
2024-03-22 02:00:25 -0300

    Update aula2.js

--- aula2.js
+++ aula2.js
@@ -1,103 +1,106 @@
 var x, y, xw, xh, sx, sy, sw, sh; // coord rect
-var xa, xb, xr;  // coord elipse
+var xa, xb, xr; // coord elipse
 var mxa, mxb; // movimento
 var ww, wh; // altura e largura da janela
-var z = 0 // 
+var z = 0 // preciso desse mesmo?
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-	ww = windowWidth
-	wh = windowHeight
+		ww = windowWidth - 100
+	wh = windowHeight - 100
 	
-  xa = ww/2 // x da elipse
-  xb = 500 // y da elipse
+	createCanvas(ww, wh);
+
+
+	xa = ww / 2 // x da elipse
+	xb = 500 // y da elipse
 	xr = 40 // largura da elipse
-	
-	mxa = 8 // movimento elipse X
-	mxb = 8 // movimento elipse y
-	
-		sx = ww/3
-sy = wh/3
-sw = ww/3
-sh = wh/3
-		
+
+	mxa = 6 // movimento elipse X
+	mxb = 6 // movimento elipse y
+
+	sx = ww / 3 // coord do botão
+	sy = wh / 3 // coord do botão
+	sw = ww / 3 // coord do botão
+	sh = wh / 6 // coord do botão
+
+	x = ww / 2 - 45; // x do rect
+	y = 524; // y do rect
+	xw = 90; // width do rect
+	xh = 20; // height do rect
 }
 
 function draw() {
-  background(70, 130, 180);
-  x = ww/2 - xr; // x do rect
-  y = 524; // y do rect
-  xw = 80; // width do rect
-  xh = 20; // height do rect
-	
+	background(70, 130, 180);
 
-  fill('#799BC5') 
+	fill('#799BC5')
 	textAlign(CENTER)
-  textSize(20);
-  text('DRAG AND PLAY!', ww/2, 50);
+	textSize(width * 0.03);
+	text('DRAG AND PLAY!', ww / 2, 50);
 
-  if(mouseIsPressed){
-    x = mouseX;
+	if (mouseIsPressed) {
+		x = mouseX; // o eixo X da barrinha passa a seguir o mouse
 	}
-		if (x < 0){
-	 x != mouseX;
+	if (x < 0) { //para a barrinha não atravessar a janela à esquerda
 		x = 0
-}
-	if (x > ww - 80){
-	 x != mouseX;
+	}
+	if (x > ww - 80) { //para a barrinha não atravessar a janela à direita
 		x = ww - 80
 	}
+	fill(255, 222, 173);
+	rect(x, y, xw, xh, 8); // retângulo móvel 
 
-  fill(255, 222, 173);
-  rect(x, y, xw, xh); // retângulo móvel 
 
+	ellipse(xa, xb, xr, xr) // elipse move enquanto mouse está pressionado, ou seja, começa o jogo
+	if (mouseIsPressed) {
+		xa = xa + mxa
+		xb = xb - mxb
+	} 
+	else { //caso não esteja jogando (mouse unpressed) então mostra 'botão' de START
 
-  ellipse(xa, xb, xr, xr) // elipse movimentando quando mouse é pressionado
-  if (mouseIsPressed){
-    xa = xa + mxa 
-    xb = xb - mxb
-  }
-	
+		fill(100)
+		rect(sx + 2, sy + 2, sw, sh, 8) //sombra do 'botão' do START
+		fill(150)
+		rect(sx, sy, sw, sh, 8) // 'botão' do START 
 
-	else{
-
-fill(150)
-rect(sx, sy, sw, sh)
-	   fill('#64D0D5');
-textAlign(CENTER)
-	   textSize(40);
-text('START', windowWidth/2, windowHeight/2)	
-	}
-	
-	if (mouseIsPressed){
-		noCursor() // cursor do mouse invisível enquanto joga
-  }
-	else { cursor()}
-
-  // se a coord x da elipse "encostar" na borda à direita ou da esquerda, inverte a movimentação no eixo X
-  if (xa > ww - 20 || xa < 20){
-    mxa = -mxa;
-  }
-
-  // se a coord y da elipse "encostar" no teto, inverte o movimento no eixo Y
-  if (xb < 20)
-    mxb = -mxb;
-
-  // condição para quando a elipse encosta no retângulo, considera a coordenada variável de ambas, tanto largura quanto altura
- if (xa + xr/2 >= x && xa - xr/2 <= x + xw && xb + xr/2 >= y && xb - xr/2 <= y + xh) {
-    mxb = -mxb;
-  }
-
-  //se a elipse ultrapassar a coordenada 600 do eixo Y (chão), faz aparecer o texto
-  if (xb > wh){
-    fill('#D57F64');
+		noStroke(0)
+		fill('#64D0D5');
 		textAlign(CENTER)
-    textSize(40);
-    text('TRY AGAIN!', windowWidth/2, windowHeight/2);
+		textSize(width * 0.05);
+		text('START', ww / 2, wh / 2 - 40)
+	}
+
+	if (mouseIsPressed) {
+		noCursor() // cursor do mouse invisível enquanto joga
+	} else {
+		cursor()
+	}
+
+	// se a coord x da elipse "encostar" na borda à direita ou da esquerda, inverte a movimentação no eixo X
+	if (xa > ww - 20 || xa < 20) {
+		mxa = -mxa;
+	}
+
+	// se a coord y da elipse "encostar" no teto, inverte o movimento no eixo Y
+	if (xb < 20) {
+		mxb = -mxb;
+	}
+	// condição para quando a elipse encosta no retângulo, considera a coordenada variável de ambas, tanto largura quanto altura
+	if (xa + xr / 2 >= x && xa - xr / 2 <= x + xw && xb + xr / 2 >= y && xb - xr / 2 <= y + xh) {
+		mxb = -mxb;
+	}
+
+	//se a elipse ultrapassar a coordenada 600 do eixo Y (chão), faz aparecer o texto
+	if (xb > wh) {
+
 	
-	
-		
-		
-  }	
+		fill(100)
+		rect(sx + 2, sy + 2, sw, sh, 8) //sombra do retângulo do TRY AGAIN
+		fill(150)
+		rect(sx, sy, sw, sh, 8) //retângulo do TRY AGAIN
+		fill('#DAAE66');
+		textAlign(CENTER)
+		textSize(width * 0.03);
+		text('TRY AGAIN!', ww / 2, wh / 2 - 40)
+	}
 
 }
